c058026ee1314f660c8e4f3732bbfce024bc06ef
donghyun-98 <ehdtkd98@korea.ac.kr>
2024-07-17 19:55:21 +0900

    [level 1] Title: 덧칠하기, Time: 22.12 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,10 +1,13 @@
 def solution(n, m, section):
     count = 0  # 페인트칠 횟수
-    current_end = 0  # 현재 롤러로 커버할 수 있는 마지막 구역
+    i = 0  # 현재 검사할 section 인덱스
 
-    for start in section:
-        if start > current_end:  # 현재 구역이 롤러로 커버되지 않으면
-            count += 1  # 새로운 페인트칠 시작
-            current_end = min(start + m - 1, n)  # 롤러의 끝 위치를 벽의 끝을 넘지 않도록 갱신
+    while i < len(section):
+        count += 1  # 새로운 페인트칠 시작
+        end = section[i] + m - 1  # 현재 롤러의 끝 위치 계산
+        
+        # 현재 롤러로 커버되는 구역의 다음 구역을 찾음
+        while i < len(section) and section[i] <= end:
+            i += 1
 
     return count
