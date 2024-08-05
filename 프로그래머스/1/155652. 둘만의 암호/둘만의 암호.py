5ca03153c81d5623e2f580006ad0d2ab9944359f
Donghyun Shin <ehdtkd98@korea.ac.kr>
2024-08-05 13:57:13 +0900

    [level 1] Title: 둘만의 암호, Time: 0.56 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -4,12 +4,12 @@ def solution(s, skip, index):
     for char in s:  # s의 각 문자에 대해
         cnt = 0
         while cnt < index:  # index 보다 작을 때
-            char = chr(ord(char) + 1)
-            if ord(char) > 122:
-                char = 'a'
-            if char not in skip:
-                cnt += 1
+            char = chr(ord(char) + 1)  # 문자를 다음 문자로 바꾸고
+            if ord(char) > 122:  # 만약 바꿨을 때 z의 아스키코드를 넘어버린다면
+                char = 'a'  # a로 바꾸기.
+            if char not in skip:  # skip 리스트에 없으면 횟수 늘리고
+                cnt += 1  
         
-        answer += char
+        answer += char  # while 문을 다 돌았다면 변환 완료된 문자열을 answer에 추가.
 
     return answer
