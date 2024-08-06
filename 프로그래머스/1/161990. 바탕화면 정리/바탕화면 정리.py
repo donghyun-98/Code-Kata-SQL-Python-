646dda4677dc058269038a9e42317c86a7c97769
Donghyun Shin <ehdtkd98@korea.ac.kr>
2024-08-06 10:42:04 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -2,10 +2,12 @@ def solution(wallpaper):
     x_list = []
     y_list = []
     
-    for i in range(len(wallpaper)):
+    # '#'이 나타나는 좌표를 찾아서 각각 x 와 y 리스트에 추가
+    for i in range(len(wallpaper)):  
         for j in range(len(wallpaper[i])):
             if wallpaper[i][j] == '#':
                 x_list.append(i)
                 y_list.append(j)
     
+    # 마지막으로 x, y 의 최솟값 | x, y 의 최대값 + 1 을 return
     return [min(x_list), min(y_list), max(x_list)+1, max(y_list)+1]
