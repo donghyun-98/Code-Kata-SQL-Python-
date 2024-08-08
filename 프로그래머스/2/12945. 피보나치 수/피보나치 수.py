91538ee1bdfe89cce3ba92b421f24ff3149fc931
Donghyun Shin <ehdtkd98@korea.ac.kr>
2024-08-08 15:26:58 +0900

    [level 2] Title: 피보나치 수, Time: 424.29 ms, Memory: 464 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,7 @@
 def solution(n):
-    memo = {0: 0, 1: 1}
+    fibo = {0: 0, 1: 1}
     
     for i in range(2, n+1):
-        memo[i] = memo[i-1] + memo[i-2]
+        fibo[i] = fibo[i-1] + fibo[i-2]
     
-    return memo[n] % 1234567
+    return fibo[n] % 1234567
