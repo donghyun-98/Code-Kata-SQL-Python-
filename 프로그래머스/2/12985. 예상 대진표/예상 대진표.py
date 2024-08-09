10f0ab9e2ecdc4faeff0c7ee52182b27e07fbe44
Donghyun Shin <ehdtkd98@korea.ac.kr>
2024-08-09 13:44:00 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,13 @@
+def solution(n,a,b):
+    answer = 0
+    ans = True
+    a_idx, b_idx = 0, 0
+    
+    # a, b가 같아질 때까지 반복
+    while a != b:
+        # a와 b에 각각 1씩 더한 후 2로 나눈 몫으로 업데이트 해주는데
+        # 그 이유는 재배정되는 번호를 표현하기 위해.
+        a, b = (a+1) // 2, (b+1) // 2
+        answer += 1
+        
+    return answer
