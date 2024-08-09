39db26ac9782602d4c5eea5358be5906b5c092c6
Donghyun Shin <ehdtkd98@korea.ac.kr>
2024-08-09 10:58:17 +0900

    [level 2] Title: 카펫, Time: 882.20 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -4,7 +4,9 @@ def solution(brown, yellow):
     
     for y in range(3, brown+ 1):  # 세로
         for x in range(y, brown+1):  # 가로
+            # x 곱하기 y 가 카펫의 전체 면적과 같고
             if x*y == carpet_size:
+                # yellow의 면적과 brown의 면적을 동시에 만족하는 x, y 조합이라면 answer에 추가
                 if ((x-2)*(y-2) == yellow) and (x*y - (x-2)*(y-2) == brown):
                     answer.append(x)
                     answer.append(y)
