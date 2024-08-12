91b0544d5203a445c6963a6450b4d4d5f990d01c
Donghyun Shin <ehdtkd98@korea.ac.kr>
2024-08-12 12:17:14 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.83 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    memo = {}
+    n = n+1
+    
+    fibo = {0: 0, 1: 1}
+    
+    for i in range(2, n+1):
+        fibo[i] = fibo[i-1] + fibo[i-2]
+    
+    return fibo[n] % 1234567
