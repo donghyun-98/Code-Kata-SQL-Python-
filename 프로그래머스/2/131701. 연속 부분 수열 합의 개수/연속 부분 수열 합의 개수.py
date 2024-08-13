df29e8181e2155ab659f35da779f76ecae78f4ed
Donghyun Shin <ehdtkd98@korea.ac.kr>
2024-08-13 12:01:45 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4470.99 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,14 +1,14 @@
 def solution(elements):
-    circle_elements = elements+elements
+    # 원형 수열 구현하기 위해 수열 두 번 붙이기
+    circle_elements = elements + elements
     answer = []
     
+    # 부분수열의 길이를 1 ~ n 까지 증가시키는 반복문
     for i in range(1, len(elements)+1):
-        for j in range(len(circle_elements)):
-            if i == len(elements):
-                answer.append(sum(elements))
-                break
-            else:
-                total = sum(circle_elements[j:j+i])
-                answer.append(total)
+        # 0부터 n-1 까지 돌면서 원형수열에서 부분수열의 합을 구해 answer에 추가하기
+        for j in range(len(elements)):
+            total = sum(circle_elements[j:j+i])
+            answer.append(total)
     
+    # 마지막에 중복을 제거하며 가능한 합의 개수를 반환
     return len(set(answer))
